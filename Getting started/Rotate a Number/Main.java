052f57a7c0f702d16082a32c4f665d282f0c873c
anamikarawat <anamikarawart@gmail.com>
2022-08-25 19:44:41 +0530

    Update Main.java

--- Getting started/Rotate a Number/Main.java
+++ Getting started/Rotate a Number/Main.java
@@ -29,3 +29,37 @@ import java.util.*;
      System.out.println(ans);
     }
 }
+
+
+// public static void main(String[] args) {
+//       Scanner scn = new Scanner(System.in);
+//       int n = scn.nextInt(), k = scn.nextInt();  
+      
+//       int temp = n ;
+//       int nod = 0 ; //number of digit
+//       while(temp>0){
+//           temp = temp/10;//decrease the size of the digit
+//           nod++; //increase the count of the digit
+//       }
+//       //suppose we find nod = 5 and k=8 then k=k%nod=3 or if k=-3 then k=k+nod=5-3=2
+//       k = k % nod;
+//       if(k < 0){
+//           k = k+ nod;
+//       }
+      
+//       int div = 1;
+//       int mult =1 ;
+//       for(int i =1; i<=nod; i++){
+//           if(i<=k){
+//               div= div*10;
+//           }else{
+//               mult = mult *10;
+//           }
+//       }
+      
+//       int quotient = n / div;
+//       int remainder = n%div;
+      
+//      int ans = remainder*mult + quotient;
+//      System.out.println(ans);
+//     }
