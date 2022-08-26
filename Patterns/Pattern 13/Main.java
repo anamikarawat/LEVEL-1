97bcf1385277422c7cc31e86882508bbf40e9d03
anamikarawat <anamikarawart@gmail.com>
2022-08-26 22:12:54 +0530

    Update Main.java

--- Patterns/Pattern 13/Main.java
+++ Patterns/Pattern 13/Main.java
@@ -24,3 +24,18 @@ public class Main{
         pattern13(n);
     }
 }
+
+// public static void main(String[] args) {
+//         Scanner scn = new Scanner(System.in);
+//         int n = scn.nextInt();
+//         for(int r =0; r<n; r++){ //for rows
+//             int val=1;
+//             for (int c=0; c<=r; c++){//for columns
+              
+//                 System.out.print(val + "\t");
+//                 val = ((r-c) * val) / (c+1);
+               
+//             }
+//             System.out.println();
+//         }
+//     }
