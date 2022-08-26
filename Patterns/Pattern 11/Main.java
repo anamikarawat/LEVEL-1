8b556c36bcf09e4ab0f57f9611b0f379dda52f6c
anamikarawat <anamikarawart@gmail.com>
2022-08-26 20:05:27 +0530

    Update Main.java

--- Patterns/Pattern 11/Main.java
+++ Patterns/Pattern 11/Main.java
@@ -20,3 +20,18 @@ public class Main {
         pattern11(n);
     }
 }
+
+//  public static void main(String[] args) {
+//         Scanner scn = new Scanner(System.in);
+//         int n = scn.nextInt();
+//         int val = 1;
+//         for( int r = 1; r <= n; r++){
+//             for( int c = 1; c<= r; c++){
+//                 System.out.print( val + "\t");
+//                  val++;
+//             }
+
+//             System.out.println();
+           
+//         }
+//     }
