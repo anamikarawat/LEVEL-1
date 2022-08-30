65666104dd4c3aafc2f062d3a31cd964a7bb88a0
anamikarawat <anamikarawart@gmail.com>
2022-08-30 14:38:21 +0530

    Update Main.java

--- Function and Arrays/Find Element In An Array/Main.java
+++ Function and Arrays/Find Element In An Array/Main.java
@@ -12,7 +12,7 @@ public class Main {
     for (int i = 0; i < n; i++) {
       arr[i] = scn.nextInt();
     }
-
+//find the element 'd' and print its position or index 'i' , if it is not present then print -1
     int d = scn.nextInt();
 
     for (int i = 0; i < arr.length; i++) {
@@ -25,3 +25,17 @@ public class Main {
   }
 
 }
+
+//============2nd method
+//find the element 'd' and print its position or index 'i' , if it is not present then print -1
+    int d = scn.nextInt();
+    int idx=-1;
+    for (int i = 0; i < arr.length; i++) {
+      if (d == arr[i]) {
+        idx = i;
+        break;
+        //break eslea kyuki hume sabse phele vala idx print karna h jo hume sabse phele mila ho
+      }
+    }
+    System.out.println(idx);
+
