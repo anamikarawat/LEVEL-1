04c0620daff5db3d1a7f2b1dc1108bdf4d96ca95
anamikarawat <anamikarawart@gmail.com>
2022-08-30 17:51:38 +0530

    Update Main.java

--- Function and Arrays/Bar Chart/Main.java
+++ Function and Arrays/Bar Chart/Main.java
@@ -5,6 +5,7 @@ import java.util.*;
 public class Main {
 
        public static int maximum(int [] arr){
+              
         int max = -(int)1e9;
         for(int i = 0; i<arr.length; i++){
             if(max< arr[i]){
@@ -37,8 +38,34 @@ public class Main {
             arr[i] = scn.nextInt();
         } 
          barChart(arr);
-
        }
-  
-
 }
+
+//============================== 2nd method
+public static void main(String [] args) {
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+    int [] arr = new int[n];
+    
+    for (int i = 0; i < n; i++) {
+      arr[i] = scn.nextInt(); //array input le liya
+    }
+    
+    int max = arr[0];
+    for (int i = 0; i < arr.length; i++) {
+      if (max < arr[i]) {
+        max = arr[i];
+      }
+    }
+    for (int h = max ; h >= 1 ; h--) {
+      for (int i = 0; i < arr.length; i++) {
+        if (arr[i] >= h) {
+          System.out.print("*\t");
+        }
+        else {
+          System.out.print("\t");
+        }
+      }
+      System.out.println();
+    }
+  }
