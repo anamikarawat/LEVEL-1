3d65af7672b148d7f5437283919a590f5ca8851b
anamikarawat <anamikarawart@gmail.com>
2022-08-30 14:23:26 +0530

    Update Main.java

--- Function and Arrays/Find Element In An Array/Main.java
+++ Function and Arrays/Find Element In An Array/Main.java
@@ -25,3 +25,27 @@ public class Main {
   }
 
 }
+ 
+//===== 2nd method
+
+ public static void main(String[] args) throws Exception {
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+    int[] arr = new int[n];
+
+    for (int i = 0; i < n; i++) {
+      arr[i] = scn.nextInt();
+    }
+
+    int min = Integer.MAX_VALUE; // value is + infinity
+    int max = Integer.MIN_VALUE; // value is - infinity
+    for(int i=0;i<n;i++){
+        max =Math.max(max,arr[i]);
+        min =Math.min(min,arr[i]);
+    }
+    
+    int span = max - min;
+    System.out.println(span);
+  }
+
+//============== 3rd method
