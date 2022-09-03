7dea5842055efed61a327927cf87f1aface3978f
anamikarawat <anamikarawart@gmail.com>
2022-09-03 19:44:59 +0530

    Update Main.java

--- 2D Arrays/Matrix Multiplication/Main.java
+++ 2D Arrays/Matrix Multiplication/Main.java
@@ -35,6 +35,9 @@ public static int [][] matrixMul(int [][]A, int [][] B){
             int sum = 0;
             for(int k = 0; k<m1; k++){
                 sum += A[i][k] * B[k][j];
+              //c11 = a11xb11 + a12xb21 + a13xb31 = sum
+              //c23 = a21xb13 + a22xb23 + a23xb33 = sum
+                
             }
             C[i][j] = sum;
         }
@@ -42,8 +45,15 @@ public static int [][] matrixMul(int [][]A, int [][] B){
     return C;
 }
 
-
-
+//A=2x3 , B=3x4
+    
+ // a11 a12 a13    &  b11  b12  b13  b14 
+ // a21 a22 a23       b21  b22  b23  b24
+ //                   b31  b32  b33  b34
+//sum= first row of A X first column of B
+    //c11  c12  c13  c14
+    //c21  c22  c23  c24
+    
 public static void main(String[] args) throws Exception {
     int n1 = scn.nextInt();
     int m1 = scn.nextInt();
