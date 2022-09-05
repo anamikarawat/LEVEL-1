b44ed44aa27c453c969f3d32389c30670387a1d2
anamikarawat <anamikarawart@gmail.com>
2022-09-05 19:29:51 +0530

    Update Main.java

--- 2D Arrays/Ring Rotate/Main.java
+++ 2D Arrays/Ring Rotate/Main.java
@@ -2,150 +2,152 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static int [] from2Dto1D(int [][] arr, int s){     //S= shell number
-        int n = arr.length;         //rows
-        int m = arr[0].length;      //col
-        
-        int minr = s-1; 
-        int maxr = n-s;
-        
-        int minc = s-1;
-        int maxc = m-s;
-        
-        int size = 2*(maxr-minr + maxc-minc);
-        
-        
-        int [] oneD = new int[size ];      //size of 1D array
-        int idx = 0;         //index
-        
-        for(int i = minr; i<=maxr; i++){      //A wall k lea minC ko fix rakh diya or minR se maxR tak travel kar diya
-            oneD[idx] = arr[i][minc];
-            idx++;          //index=1
-        }
-        minc++;
-        
-        for(int j = minc; j<= maxc; j++){      //B wall
-            oneD[idx] = arr[maxr][j];
-            idx++;      //index=2
-        }
-        
-        maxr--;
-        
-        for(int i = maxr; i>=minr; i--){   //C WALL
-            oneD[idx] = arr[i][maxc];
-            idx++;
-        }
-        maxc--;
-        
-        for(int j = maxc; j>=minc; j--){     //D WALL
-            oneD[idx] = arr[minr][j];
-            idx++;
-        }
-        minr++;
-        
-        return oneD;
-        
+  //1. convert from 2d to 1d
+  public static int [] from2Dto1D(int [][] arr, int s) {    //S= shell number
+    int n = arr.length;         //rows
+    int m = arr[0].length;      //col
+
+    int minr = s - 1;
+    int maxr = n - s;
+
+    int minc = s - 1;
+    int maxc = m - s;
+
+    int size = 2 * (maxr - minr + maxc - minc);
+
+
+    int [] oneD = new int[size ];      //size of 1D array
+    int idx = 0;         //index
+
+    for (int i = minr; i <= maxr; i++) {  //left wall k lea minC ko fix rakh diya or minR se maxR tak travel kar diya
+      oneD[idx] = arr[i][minc];
+      idx++;          //index=1
     }
-    // 2.ROTATE
-   public static void swap( int [] arr, int i , int j){
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-    }
-    public static void reverse(int [] arr, int i, int j){
-         while(i<=j){
-        swap(arr,i,j);
-        i++;   
-        j--;
-    }
+    minc++; //corner se ek step agea taki number repeat na print ho
+
+    for (int j = minc; j <= maxc; j++) {   //bottom wall k lea maxr ko fix rakh diya or minc to maxc tak travel kar diya
+      oneD[idx] = arr[maxr][j];
+      idx++;      //index=2
     }
 
-    public static void  rotate(int [] arr, int r){
-        r%=arr.length;
-        if(r<0){
-            r = r + arr.length;
-        }
-        reverse(arr, 0, arr.length-r-1);
-        reverse(arr, arr.length-r, arr.length-1);
-        reverse(arr, 0 , arr.length-1);
-        
+    maxr--;//corner se ek step agea taki number repeat na print ho
+
+    for (int i = maxr; i >= minr; i--) { //right WALL k lea maxc fix hai or maxr to minr tak travel
+      oneD[idx] = arr[i][maxc];
+      idx++;
     }
-    
-    
-    public static void from1Dto2D(int [] oneD, int [][] arr, int s){
-        int n = arr.length;         //rows
-        int m = arr[0].length;      //col
-        
-        int minr = s-1; 
-        int maxr = n-s;
-        
-        int minc = s-1;
-        int maxc = m-s;
-        
-        int idx = 0;
-        
-        for(int i = minr; i<=maxr; i++){
-            arr[i][minc] = oneD[idx];
-            idx++;
-        }
-        minc++;
-        
-        for(int j = minc; j<= maxc; j++){
-            arr[maxr][j]=oneD[idx];
-            idx++;
-        }
-        
-        maxr--;
-        
-        for(int i = maxr; i>=minr; i--){
-            arr[i][maxc]=oneD[idx];
-            idx++;
-        }
-        maxc--;
-        
-        for(int j = maxc; j>=minc; j--){
-            arr[minr][j] = oneD[idx];
-            idx++;
-        }
-        minr++;
-        
-       //return type - void hai kyuki humea print nhi karana h only 1D se 2D mea dalna h
-    
+    maxc--;//corner se ek step agea taki number repeat na print ho
+
+    for (int j = maxc; j >= minc; j--) { //top WALL k lea minr fix hai or maxc to minc tak travel
+      oneD[idx] = arr[minr][j];
+      idx++;
     }
-    public static void ringRotate(int [][] arr, int s, int r){
-        //from 2d to 1d
-        //rotate
-        //from 1D to 2D
-        
-        int [] oneD = from2Dto1D(arr, s);
-        rotate(oneD , r);
-        from1Dto2D(oneD, arr,s);
+    minr++;//corner se ek step agea taki number repeat na print ho
+
+    return oneD;
+
+  }
+  // 2.ROTATE 1D array
+  public static void swap( int [] arr, int i, int j) {
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+  }
+  public static void reverse(int [] arr, int i, int j) {
+    while (i <= j) {
+      swap(arr, i, j);
+      i++;
+      j--;
     }
-    
-    
-    public static void main(String[] args) throws Exception {
-        Scanner scn = new Scanner(System.in);
-        int n = scn.nextInt();
-        int m = scn.nextInt();
-        int [][] arr = new int[n][m];
-        for(int i = 0; i<n; i++){
-            for(int j = 0; j<m; j++){
-                arr[i][j] = scn.nextInt();
-            }
-        }
-        int s = scn.nextInt();     //input
-        int r = scn.nextInt();
-        ringRotate(arr,s,r);
-        display(arr);
+  }
+
+  public static void  rotate(int [] arr, int r) {
+    r %= arr.length;
+    if (r < 0) {
+      r = r + arr.length;
+    }
+    reverse(arr, 0, arr.length - r - 1);
+    reverse(arr, arr.length - r, arr.length - 1);
+    reverse(arr, 0, arr.length - 1);
+
+  }
+
+  //3. convert from 2d to 1d
+  public static void from1Dto2D(int [] oneD, int [][] arr, int s) {
+    int n = arr.length;         //rows
+    int m = arr[0].length;      //col
+
+    int minr = s - 1;
+    int maxr = n - s;
+
+    int minc = s - 1;
+    int maxc = m - s;
+
+    int idx = 0;
+
+    for (int i = minr; i <= maxr; i++) {
+      arr[i][minc] = oneD[idx];
+      idx++;
+    }
+    minc++;
+
+    for (int j = minc; j <= maxc; j++) {
+      arr[maxr][j] = oneD[idx];
+      idx++;
     }
 
-    public static void display(int[][] arr){
-        for(int i = 0; i < arr.length; i++){
-            for(int j = 0; j < arr[0].length; j++){
-                System.out.print(arr[i][j] + " ");
-            }
-            System.out.println();
-        }
+    maxr--;
+
+    for (int i = maxr; i >= minr; i--) {
+      arr[i][maxc] = oneD[idx];
+      idx++;
     }
+    maxc--;
+
+    for (int j = maxc; j >= minc; j--) {
+      arr[minr][j] = oneD[idx];
+      idx++;
+    }
+    minr++;
+
+    //return type - void hai, kyuki humea print nhi karana h only 1D se 2D mea dalna h
+
+  }
+
+  public static void ringRotate(int [][] arr, int s, int r) {
+    //from 2d to 1d
+    //rotate
+    //from 1D to 2D
+
+    int [] oneD = from2Dto1D(arr, s);
+    rotate(oneD, r);
+    from1Dto2D(oneD, arr, s);
+  }
+
+  public static void main(String[] args) throws Exception {
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+    int m = scn.nextInt();
+    int [][] arr = new int[n][m];
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < m; j++) {
+        arr[i][j] = scn.nextInt();
+      }
+    }
+    int s = scn.nextInt();     //input
+    int r = scn.nextInt();
+
+    ringRotate(arr, s, r);
+    display(arr);
+  }
+
+  public static void display(int[][] arr) {
+    for (int i = 0; i < arr.length; i++) {
+      for (int j = 0; j < arr[0].length; j++) {
+        System.out.print(arr[i][j] + " ");
+      }
+      System.out.println();
+    }
+  }
 
 }
