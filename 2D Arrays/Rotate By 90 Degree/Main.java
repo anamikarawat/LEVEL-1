82dd439e176c99f5018ab57d9d471ad414520c81
anamikarawat <anamikarawart@gmail.com>
2022-09-05 16:44:05 +0530

    Update Main.java

--- 2D Arrays/Rotate By 90 Degree/Main.java
+++ 2D Arrays/Rotate By 90 Degree/Main.java
@@ -80,3 +80,57 @@ public class Main {
     }
 
 }
+
+//====================================================
+public class Main {
+  public static void main(String[] args) throws Exception {
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+    int [][] arr = new int[n][n];
+
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+        arr[i][j] = scn.nextInt();
+      }
+    }
+
+    // 2 steps
+
+    // 1. Transpose
+    for (int i = 0; i < n ; i++) {     //transpose the whole matrix--to get orginal matrix
+      for (int j = i; j < n; j++) {  //j=i transpose the upper half matrix
+        int tmp = arr[i][j];
+        arr[i][j] = arr[j][i];
+        arr[j][i] = tmp;
+      }
+    }
+    // 2. Reverse each row
+    for (int i = 0; i < n ; i++) {
+      int li = 0;
+      int ri = n - 1;
+
+      while (li <= ri) {
+        int tmp = arr[i][li];
+        arr[i][li] = arr[i][ri];
+        arr[i][ri] = tmp;
+
+        li++;
+        ri--;
+      }
+
+    }
+
+    display(arr);
+  }
+
+  public static void display(int[][] arr) {
+    for (int i = 0; i < arr.length; i++) {
+      for (int j = 0; j < arr[0].length; j++) {
+        System.out.print(arr[i][j] + " ");
+      }
+      System.out.println();
+    }
+  }
+
+
+}
