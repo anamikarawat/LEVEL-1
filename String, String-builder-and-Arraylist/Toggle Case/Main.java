b4765bbd10d8602beaee0968b8a094259c270989
anamikarawat <anamikarawart@gmail.com>
2022-09-07 01:49:39 +0530

    Update Main.java

--- String, String-builder-and-Arraylist/Toggle Case/Main.java
+++ String, String-builder-and-Arraylist/Toggle Case/Main.java
@@ -1,38 +1,37 @@
 import java.io.*;
 import java.util.*;
-
-
 public class Main {
+  public static String toggleCase(String str) {
 
-	public static String toggleCase(String str){
-		
- StringBuilder sb = new StringBuilder(str);
- //StringBuilder ans = new StringBuilder(str);
+    StringBuilder sb = new StringBuilder(str);
+  //humea character CHANGE karne hai toh hum stringbuilder use karege and also StringBuilder is faster than string
     for (int i = 0 ; i < sb.length() ; i++) {
       char ch = sb.charAt(i);
-      //  for (int i = 0 ; i < str.length() ; i++) {
-      // char ch = str.charAt(i);
 
       if (ch >= 'A' && ch <= 'Z') {
         // convert to lowerCase
         char lc = ( char ) ( ch - 'A' + 'a' );
         sb.setCharAt(i, lc);
-        //ans.append(lc);
+
       }
       else if (ch >= 'a' && ch <= 'z') {
         // convert to UpperCase
         char uc = (char) (ch - 'a' + 'A');
         sb.setCharAt(i, uc);
-         //ans.append(uc);
+
       }
     }
-    return sb.toString();
-   // return ans.toString();    --->convert stringbuilder to string
+    return sb.toString(); //convert stringbuilder to string
+
   }
-	public static void main(String[] args) {
-		Scanner scn = new Scanner(System.in);
-		String str = scn.next();
-		System.out.println(toggleCase(str));
-	}
+  public static void main(String[] args) {
+    Scanner scn = new Scanner(System.in);
+    String str = scn.next();
+    System.out.println(toggleCase(str));
+  }
 
 }
+
+//also we have formulas->
+//Uppercase = lowercase - 'a' + 'A'
+//lowercase = uppercase -'A' + a
