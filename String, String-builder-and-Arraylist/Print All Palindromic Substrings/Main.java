1f789e2daede2a7a25a00c8c5f25cf5d1aed9eaa
anamikarawat <anamikarawart@gmail.com>
2022-09-06 14:41:21 +0530

    Update Main.java

--- String, String-builder-and-Arraylist/Print All Palindromic Substrings/Main.java
+++ String, String-builder-and-Arraylist/Print All Palindromic Substrings/Main.java
@@ -2,24 +2,12 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
-	public static void solution(String str){
-		
-		   for (int i = 0 ; i < str.length(); i++) {
+  public static void solution(String str){
+	for (int i = 0 ; i < str.length(); i++) {
         for (int j = i + 1; j <= str.length(); j++) {
         // i -> starting point of substring, j-> ending point of substring
         if (isPalindrome(str.substring(i, j))) {
           System.out.println(str.substring(i, j));
-
-          //int n = s.length;
-          //for(int st=0; st<n; st++){
-           //   for(int end = st; end<n;end++) {
-             //     String subS= s.substring(st,end+1);
-              //    if(ispalindrome(subS)== true)
-              //    System.out.println(subS);
-            //  }
-           //  }
-
         }
       }
     }
@@ -38,10 +26,50 @@ public class Main {
   }
 	
 
-	public static void main(String[] args) {
-		Scanner scn = new Scanner(System.in);
-		String str = scn.next();
-		solution(str);
-	}
+  public static void main(String[] args) {
+	Scanner scn = new Scanner(System.in);
+	String str = scn.next();
+	solution(str);
+   }
+
+}
+
+//====================================================================================
+public class Main {
+
+  public static void solution(String str) {
+    for (int i = 0 ; i < str.length(); i++) {
+      for (int j = i + 1; j <= str.length(); j++) {
+        // i -> starting point of substring, j-> ending point of substring
+        String s = str.substring(i, j);
+        if (isPalindrome(s) == true) {
+          System.out.println(s);
+        }
+      }
+    }
+  }
+
+  public static boolean isPalindrome(String str) {
+    int i = 0, j = str.length() - 1;
+    while (i <= j) {
+        
+      char ch1 = str.charAt(i);
+      char ch2 = str.charAt(j);
+      
+      if (ch1 != ch2) {
+        return false;
+      }else{
+       i++;
+       j--;
+      }      
+    }
+    return true;
+  }
+
+  public static void main(String[] args) {
+    Scanner scn = new Scanner(System.in);
+    String str = scn.next();
+    solution(str);
+  }
 
 }
