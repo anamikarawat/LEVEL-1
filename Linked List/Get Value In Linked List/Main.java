fd2d7be50d2c997c45630e133d9fcb657bb65027
anamikarawat <anamikarawart@gmail.com>
2022-09-09 20:53:00 +0530

    Update Main.java

--- Linked List/Get Value In Linked List/Main.java
+++ Linked List/Get Value In Linked List/Main.java
@@ -49,7 +49,7 @@ public class Main {
         size--;
       }
     }
-
+//================code==============
     public int getFirst() {
       if (size == 0) {
         System.out.println("List is empty");
@@ -85,7 +85,7 @@ public class Main {
       }
     }
   }
-
+//==============================================
 
   public static void main(String[] args) throws Exception {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
