9264a1a3a0b861c596735991325060953c681a98
anamikarawat <anamikarawart@gmail.com>
2022-09-14 01:30:51 +0530

    Create Factorial.java

---
+++ Recursion/Introduction to Recursion/Factorial.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+  public static void main(String[] args) throws Exception {
+    // write your code here
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int ans = factorial(n);
+    System.out.println(ans);
+  }
+
+  public static int factorial(int n) {
+    if (n == 1) {
+      return 1;
+    }
+    int recAns = factorial(n - 1);
+    int myAns = n * recAns;
+
+    return myAns;
+  }
+}
