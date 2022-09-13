fcd506d463d44e3fe8efcb7ed5397bed2280a388
anamikarawat <anamikarawart@gmail.com>
2022-09-13 23:53:31 +0530

    Update Main.java

--- Recursion/Introduction to Recursion/Print Increasing/Main.java
+++ Recursion/Introduction to Recursion/Print Increasing/Main.java
@@ -10,11 +10,11 @@ public class Main {
            printIncreasing(n);
         }
 
-    public static void printIncreasing(int n){
+    public static void printIncreasing(int n){ //n=5
         if(n==0){
             return;
         }
-        
+        //output = 1 2 3 4 5
         printIncreasing(n-1);
         System.out.println(n);
     }
