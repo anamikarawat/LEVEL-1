4acc7318ef596402a9d6fdcde178d1a451cf2ecf
anamikarawat <anamikarawart@gmail.com>
2022-09-27 22:44:40 +0530

    imp

--- Recursion/Recursion with Arraylists/Get Subsequence/Main.java
+++ Recursion/Recursion with Arraylists/Get Subsequence/Main.java
@@ -5,12 +5,15 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
       Scanner scn = new Scanner(System.in);
+        
       String str = scn.next();
       ArrayList<String> res = gss(str);
-      System.out.println(res);
+        //gss = get subsequence
+      System.out.println(res); //res = result
     }
 
     public static ArrayList<String> gss(String str) {
+        //base case
         if(str.length() == 0){
             ArrayList<String> base = new ArrayList<>();
             base.add("");
@@ -18,20 +21,27 @@ public class Main {
             
         }
         
-        
+        //faith = bc-> [" " , "c" , "b" , "bc" ]
+        //expectation = abc -> [" " , "c" , "b" , "bc" , "a" , "ac" ,"ab" ,"abc" ]
         char ch = str.charAt(0);        //a
         String ros = str.substring(1);      //bc
+        //ros = rest of string
         
-        ArrayList<String> recAns = gss(ros);
-        ArrayList<String> myAns = new ArrayList<>(); 
+        ArrayList<String> recAns = gss(ros); //faith
+        //recAns = recursion answer
+        
+        ArrayList<String> myAns = new ArrayList<>(); //expectation
         
        
         for(String ele : recAns){
-            myAns.add(ele);
+            //[" " , "c" , "b" , "bc" ]
+            myAns.add(ele); //nothing to add
+           
         }
         
          for(String ele : recAns){
-            myAns.add(ch+ele);
+            myAns.add(ch + ele); // add first character
+             // ["a" , "ac" ,"ab" ,"abc" ]
         }
         
         return myAns;
